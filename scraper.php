2f9fc1e4c865107b7ffcd3623899431c08b778b6
Tom Chmielarz <tom.chmielarz@gmail.com>
2018-03-10 12:57:28 -0500

    Add files via upload

---
+++ scraper.php
@@ -0,0 +1,22 @@
+<?php
+
+//
+// A super simple PHP example that scrapes a webpage
+//
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"http://www.tomchmielarz.com/scrape_test.php");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+$output = curl_exec ($ch);
+$info = curl_getinfo($ch);
+
+curl_close ($ch);
+
+if ($info['http_code'] == 200){
+	//TODO: extract data
+}
+
+?>
